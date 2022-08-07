3d226a6253ad06b10edbbdeeacc26a0adefc1b6f
GBAleksandrGB <tomilov.alex@gmail.com>
2022-08-07 12:22:28 +0300

    completing lecture assignment 2

--- library/library/settings.py
+++ library/library/settings.py
@@ -40,16 +40,22 @@ INSTALLED_APPS = [
     'rest_framework',
     'authors',
     'users',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+        'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'library.urls'
